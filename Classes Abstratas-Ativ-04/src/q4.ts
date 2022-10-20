227ee59f490a7c63f1a1cb6cbf7d0f5b0b90402d
MathLevi20 <matheuslevi545@gmail.com>
2022-10-20 02:41:16 -0300

    feat: Classes Abstratas

---
+++ Classes Abstratas-Ativ-04/src/q4.ts
@@ -0,0 +1,51 @@
+﻿abstract class FiguraGeometrica {
+    abstract calcula_Area(): void;
+    abstract calcula_Perimetro(): void;
+}
+class Quadrado extends FiguraGeometrica {
+    l1: number
+    l2: number
+    constructor(base: number, altura: number) {
+        super()
+        this.l1 = base
+        this.l2 = altura
+    }
+    calcula_Area(): number {
+        return this.l1 * this.l2;
+    }
+    calcula_Perimetro(): void {
+        this.l1 * 2 + this.l2 * 2
+    }
+}
+
+class Triangulo extends FiguraGeometrica {
+    base: number
+    altura: number
+    constructor(base: number, altura: number) {
+        super()
+        this.base = base
+        this.altura = altura
+    }
+    calcula_Area(): number {
+        return this.base * this.altura;
+    }
+    calcula_Perimetro(): number {
+        return this.base * 3
+    }
+}
+
+
+class Circulo extends FiguraGeometrica {
+    raio: number
+    constructor(raio: number) {
+        super()
+        this.raio = raio
+    }
+    calcula_Area(): number {
+        return 3.14 * this.raio ** 2
+    }
+    calcula_Perimetro(): number {
+        return 2 * 3.14 * this.raio;
+    }
+}
+
---
+++ Classes Abstratas-Ativ-04/src/q6.ts
@@ -0,0 +1,47 @@
+﻿/*
+1- Sim, classes abstratas surgem do desconhecimento da implementação de um trecho específico de uma classe, de modo que ao instanciá-las, deve-se especificar o comportamento do trecho abstrato.
+2- É necessário que o método ImprimaAlgo() da superclasse seja declarado.
+3- A subclasse é obrigada a sobrescrever o método da superclasse abstrata.
+*/
+
+/*
+5- porquê o vetor não precisa inicializar a classe
+*/
+
+
+//6------------------
+abstract class Funcionario {
+    abstract getbonificacao(): void;
+}
+
+class Gerente extends Funcionario {
+    salario: number
+    constructor(salario: number) {
+        super()
+        this.salario = salario;
+    }
+    getbonificacao(): number {
+        return this.salario * 0.4
+    }
+}
+class Diretor extends Funcionario {
+    salario: number
+    constructor(salario: number) {
+        super()
+        this.salario = salario;
+    }
+    getbonificacao(): number {
+        return this.salario * 0.6
+    }
+}
+
+class Presidente extends Funcionario {
+    salario: number
+    constructor(salario: number) {
+        super()
+        this.salario = salario;
+    }
+    getbonificacao(): number {
+        return this.salario * 1
+    }
+}
