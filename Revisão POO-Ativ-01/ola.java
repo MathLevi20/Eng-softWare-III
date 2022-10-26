e25331f3c7f065c5738f9c4fce91c77019e94099
MathLevi20 <matheuslevi545@gmail.com>
2022-10-26 01:45:31 -0300

    feat: Atividade 01

---
+++ Revisão POO-Ativ-01/ola.java
@@ -0,0 +1,45 @@
+﻿class Conta {
+
+private long numero;
+
+private double saldo;
+
+void debito (double valor) {
+
+saldo = saldo - valor;
+
+}
+
+...
+
+}
+class Conta {
+private long numero;
+private double saldo;
+
+void debito (double valor) {
+if (valor <= saldo)
+saldo = saldo - valor;
+else
+System.out.print("Saldo Insuficiente!");
+}
+
+...
+
+}
+class Conta {
+
+private long numero;
+private double saldo;
+
+boolean debito (double valor) {
+if (valor <= saldo) {
+saldo = saldo - valor;
+return true;
+} else
+return false;
+}
+
+...
+
+}
